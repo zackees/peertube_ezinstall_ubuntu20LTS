f57a8dc7be8bc35681f9148a95080fca2c26e903
zackees <z@zackees.com>
2022-02-09 19:29:10 -0800

    refactor chdir for logging

--- install_demo.py
+++ install_demo.py
@@ -26,6 +26,10 @@ def system_name() -> str:
 def system_version() -> str:
     return platform.uname().version
 
+def chdir(path: str) -> str:
+    prev = os.chdir()
+    os.chdir(path)
+    return prev
 
 def main() -> None:
     if 'ubuntu' != system_name():
@@ -45,7 +49,7 @@ def main() -> None:
     exec_shell("sudo systemctl start redis postgresql")
     exec_shell("sudo useradd -m -d /var/www/peertube -s /bin/bash -p peertube peertube")
     exec_shell("echo peertube | sudo passwd peertube")
-    os.chdir("/var/www/peertube")
+    chdir("/var/www/peertube")
     exec_shell(
         "sudo -u postgres createdb -O peertube -E UTF8 -T template0 peertube_prod")
     exec_shell('sudo -u postgres psql -c "CREATE EXTENSION pg_trgm;" peertube_prod')
@@ -53,26 +57,26 @@ def main() -> None:
     version_str = exec_stdout(
         '''curl -s https://api.github.com/repos/chocobozzz/peertube/releases/latest | grep tag_name | cut -d '"' -f 4''',
     ).strip()
-    os.chdir("/var/www/peertube")
+    chdir("/var/www/peertube")
     exec_shell("sudo -u peertube mkdir -p config storage versions")
     exec_shell("sudo -u peertube chmod 750 config/")
-    os.chdir("/var/www/peertube/versions")
+    chdir("/var/www/peertube/versions")
     exec_shell(
         f'sudo -u peertube wget -q "https://github.com/Chocobozzz/PeerTube/releases/download/{version_str}/peertube-{version_str}.zip"'
     )
     exec_shell(
         f"sudo -u peertube unzip -q peertube-${version_str}.zip && sudo -u peertube rm peertube-${version_str}.zip"
     )
-    os.chdir("/var/www/peertube")
+    chdir("/var/www/peertube")
     exec_shell(
         f"sudo -u peertube ln -s versions/peertube-${version_str} ./peertube-latest")
-    os.chdir("./peertube-latest")
+    chdir("./peertube-latest")
     exec_shell("sudo -H -u peertube yarn install --production --pure-lockfile")
-    os.chdir("/var/www/peertube")
+    chdir("/var/www/peertube")
     exec_shell(
         "sudo -u peertube cp peertube-latest/config/default.yaml config/default.yaml"
     )
-    os.chdir("/var/www/peertube")
+    chdir("/var/www/peertube")
     exec_shell(
         "sudo -u peertube cp peertube-latest/config/production.yaml.example config/production.yaml"
     )
