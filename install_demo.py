11ba1c475e714d1c1769ff5b0c5453383065ecda
zackees <z@zackees.com>
2022-02-09 18:49:30 -0800

    updates install to work on linux

--- install_demo.py
+++ install_demo.py
@@ -3,12 +3,23 @@ import sys
 import subprocess
 
 
-def exec_shell(cmd: str):
+def exec_shell(cmd: str) -> int:
     print(f"Running {cmd}:")
     rtn = os.system(cmd)
     if rtn != 0:
         print(f"Warning, executing \n  {cmd}\n  returned code {rtn}")
+    return rtn
 
+
+def exec_stdout(cmd: str) -> str:
+    return subprocess.check_output(cmd, shell=True)
+
+
+if True:  # Debug
+    version_str = exec_stdout(
+        '''curl -s https://api.github.com/repos/chocobozzz/peertube/releases/latest | grep tag_name | cut -d '"' -f 4''',
+    ).strip()
+    sys.exit(1)
 
 exec_shell("sudo apt update")
 exec_shell("sudo apt install python3-dev python-is-python3")
@@ -22,13 +33,13 @@ exec_shell("sudo systemctl start redis postgresql")
 exec_shell("sudo useradd -m -d /var/www/peertube -s /bin/bash -p peertube peertube")
 exec_shell("sudo passwd peertube")
 os.chdir("/var/www/peertube")
-exec_shell("sudo -u postgres createdb -O peertube -E UTF8 -T template0 peertube_prod")
+exec_shell(
+    "sudo -u postgres createdb -O peertube -E UTF8 -T template0 peertube_prod")
 exec_shell('sudo -u postgres psql -c "CREATE EXTENSION pg_trgm;" peertube_prod')
 exec_shell('sudo -u postgres psql -c "CREATE EXTENSION unaccent;" peertube_prod')
-version_str = subprocess.check_output(
-    '''VERSION=$(curl -s https://api.github.com/repos/chocobozzz/peertube/releases/latest | grep tag_name | cut -d '"' -f 4) && echo " $VERSION"'''
-    shell=True,
-)
+version_str = exec_stdout(
+    '''curl -s https://api.github.com/repos/chocobozzz/peertube/releases/latest | grep tag_name | cut -d '"' -f 4''',
+).strip()
 os.chdir("/var/www/peertube")
 exec_shell("sudo -u peertube mkdir config storage versions")
 exec_shell("sudo -u peertube chmod 750 config/")
@@ -40,7 +51,8 @@ exec_shell(
     f"sudo -u peertube unzip -q peertube-${version_str}.zip && sudo -u peertube rm peertube-${version_str}.zip"
 )
 os.chdir("/var/www/peertube")
-exec_shell(f"sudo -u peertube ln -s versions/peertube-${version_str} ./peertube-latest")
+exec_shell(
+    f"sudo -u peertube ln -s versions/peertube-${version_str} ./peertube-latest")
 os.chdir("./peertube-latest")
 exec_shell("sudo -H -u peertube yarn install --production --pure-lockfile")
 os.chdir("/var/www/peertube")
