036b97f33787e58020afa5b91eb40a65d483f233
zackees <z@zackees.com>
2022-02-09 18:37:14 -0800

    nit

--- install_demo.sh
+++ install_demo.sh
@@ -1,4 +1,1 @@
-# peertube_ezinstall_ubuntu20LTS
-Easily installs peertube on ubuntu20LTS as a fresh install
-
 curl https://raw.githubusercontent.com/zackees/peertube_ezinstall_ubuntu20LTS/main/install_demo.py | python3
